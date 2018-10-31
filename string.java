94dc323ca302570f6b391ed7d19d123ab212960d
ashika0898 <35723847+ashika0898@users.noreply.github.com>
2018-10-31 12:27:43 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string{  
+   public static void main(String args[]){   
+	String str = "Beginnersbook"; 
+	char arrch[]={'h','e','l','l','o'}; 
+	String str2 = new String(arrch); 
+	String str3 = new String("Java String string"); 
+	System.out.println(str);  
+	System.out.println(str2);  
+	System.out.println(str3);  
+   }
+}
